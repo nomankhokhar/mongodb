d049f2646b8865a97cc48d154cd2e664444f5f7d
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2023-09-12 13:05:52 +0500

    Connect mongo DB

--- MongoDbNOdeJS/app.js
+++ MongoDbNOdeJS/app.js
@@ -1,15 +1,23 @@
 const express = require('express')
+const { connectToDb, getDb } = require('./db')
+
 const app = express()
 
+let db
 
-
-
-app.get("/books", (req , res) => {
-    res.json({msgg : "Welcome to API LOL"})
+connectToDb((err) => {
+    if (!err) {
+        app.listen(3000, () => {
+            console.log("App listening on port 3000")
+        });
+        db = getDb()
+    }
 })
 
 
+app.get("/books", (req, res) => {
+    db.collection('books').find()
+    res.json({ msgg: "Welcome to API LOL" })
+})
 
-app.listen(3000 , () => {
-    console.log("App listening on port 3000")
-});
+
