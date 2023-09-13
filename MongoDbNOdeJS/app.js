92f8940461d4a56c7a753549b87fe6fa177a4120
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2023-09-13 20:40:50 +0500

    Add Multiple and Single End POint

--- MongoDbNOdeJS/app.js
+++ MongoDbNOdeJS/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { connectToDb, getDb } = require('./db')
+const { ObjectId } = require('mongodb')
 
 const app = express()
 
@@ -16,8 +17,28 @@ connectToDb((err) => {
 
 
 app.get("/books", (req, res) => {
-    db.collection('books').find()
-    res.json({ msgg: "Welcome to API LOL" })
+    let books = []
+
+    db.collection('books')
+        .find()
+        .sort({ author: 1 })
+        .forEach(book => books.push(book))
+        .then(() => res.status(200).json(books))
+        .catch(() => {
+            res.status(500).json({ error: "Could not fetch the documents" })
+        })
 })
 
-
+app.get("/books/:id", (req, res) => {
+    const id = req.params.id
+    if (ObjectId.isValid(id)) {
+        db.collection('books').findOne({ _id: new ObjectId(id) })
+            .then((book) => res.status(200).json(book))
+            .catch(() => {
+                res.status(500).json({ error: "Could not fetch the document" })
+            })
+    }
+    else {
+        res.status(301).json({ error: "id is not valid" })
+    }
+})
