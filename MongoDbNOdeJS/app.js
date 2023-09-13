fa57ae814e7f38a23785cce2adb56d2afce0f8d4
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2023-09-13 20:50:45 +0500

    All API Done

--- MongoDbNOdeJS/app.js
+++ MongoDbNOdeJS/app.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const { connectToDb, getDb } = require('./db')
 const { ObjectId } = require('mongodb')
+const cors = require('cors');
 
 const app = express()
+app.use(express.json())
+app.use(cors({
+    origin: 'http://localhost:8080',
+}));
+
 
 let db
 
@@ -19,9 +25,14 @@ connectToDb((err) => {
 app.get("/books", (req, res) => {
     let books = []
 
+    const page = req.query.p || 0
+    const booksPerPage = 3
+
     db.collection('books')
         .find()
         .sort({ author: 1 })
+        .skip(page * booksPerPage)
+        .limit(booksPerPage)
         .forEach(book => books.push(book))
         .then(() => res.status(200).json(books))
         .catch(() => {
@@ -40,5 +51,60 @@ app.get("/books/:id", (req, res) => {
     }
     else {
         res.status(301).json({ error: "id is not valid" })
+    }
+})
+
+
+
+
+
+app.post('/books', (req, res) => {
+    const book = req.body
+
+    db.collection('books')
+        .insertOne(book)
+        .then(result => {
+            res.status(201).json(result)
+        })
+        .catch((err) => {
+            res.status(500).json({ err: err })
+        })
+})
+
+
+
+
+app.delete('/books/:id', (req, res) => {
+
+    if (ObjectId.isValid(req.params.id)) {
+
+        db.collection('books')
+            .deleteOne({ _id: new ObjectId(req.params.id) })
+            .then(result => {
+                res.status(200).json(result)
+            })
+            .catch(err => {
+                res.status(500).json({ error: 'Could not delete document' })
+            })
+
+    } else {
+        res.status(500).json({ error: 'Could not delete document' })
+    }
+})
+
+
+app.patch('/books/:id', (req, res) => {
+    const updates = req.body
+
+    if (ObjectId.isValid(req.params.id)) {
+        db.collection('books').updateOne({ _id: new ObjectId(req.params.id) }, { $set: updates })
+            .then(result => {
+                res.status(200).json(result)
+            })
+            .catch(err => {
+                res.status(500).json({ error: err })
+            })
+    } else {
+        res.status(500).json({ error: 'Could not update document' })
     }
 })
