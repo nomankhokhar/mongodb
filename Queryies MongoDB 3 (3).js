c956559c33be90a9f77a40800e8d06c062098306
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2023-10-19 21:38:57 +0500

    Add Bug

--- Queryies MongoDB 3 (3).js
+++ Queryies MongoDB 3 (3).js
@@ -1,24 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost/mongo-exercises');
+mongoose.connect("mongodb://localhost/mongo-exercises");
 
 const courseSchema = new mongoose.Schema({
   name: String,
-  author: String, 
-  tags: [ String ],
-  date: Date, 
+  author: String,
+  tags: [String],
+  date: Date,
   isPublished: Boolean,
-  price: Number
+  price: Number,
 });
 
-const Course = mongoose.model('Course', courseSchema);
+const Course = mongoose.model("Course", courseSchema);
 
 async function getCourses() {
-  return await Course
-  .find({ isPublished: true })
-  .or([ { tags: 'frontend' }, { tags: 'backend' } ])
-  .sort('-price')
-  .select('name author price');
+  return await Course.find({ isPublished: true })
+    .or([{ tags: "frontend" }, { tags: "backend" }])
+    .sort("-price")
+    .select("name author price");
 }
 
 async function run() {
